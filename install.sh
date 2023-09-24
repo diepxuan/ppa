6ad8ee76a9dd9039d7add786ff1a261917d4ecad
Trần Ngọc Đức <caothu91@gmail.com>
2023-09-24 12:59:23 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,15 @@
+sudo grep -r "deb http://ppa.launchpad.net/ondrej/php/ubuntu" /etc/apt/sources.list /etc/apt/sources.list.d/*.list >/dev/null 2>&1 && return
+
+command -v add-apt-repository >/dev/null 2>&1 && (
+  sudo add-apt-repository ppa:ondrej/php -y
+) || (
+  cat <<EOF | sudo tee /etc/apt/sources.list.d/ondrej-ubuntu-php-focal.list >/dev/null &&
+deb http://ppa.launchpad.net/ondrej/php/ubuntu focal main
+# deb-src http://ppa.launchpad.net/ondrej/php/ubuntu focal main
+EOF
+  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4F4EA0AAE5267A6C
+)
+
 # sudo add-apt-repository ppa:caothu91/ppa
 
 ### Install public key for ppa.launchpad.net
