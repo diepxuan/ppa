941da8c10591d0a350d1662204a4aff8588710b3
Trần Ngọc Đức <caothu91@gmail.com>
2025-01-10 08:43:29 +0700

    add key gpg

--- install.sh
+++ install.sh
@@ -10,8 +10,8 @@ CODENAME=${CODENAME:-$UBUNTU_CODENAME}
 CODENAME=${CODENAME:-"unknown"}
 
 # Xác định URL của repository
-REPO_URL="https://raw.githubusercontent.com/diepxuan/ppa"
-# KEY_URL="$REPO_URL/public.key"
+REPO_URL="https://ppa.diepxuan.com/"
+KEY_URL="$REPO_URL/key.gpg"
 
 # Kiểm tra xem có hỗ trợ phiên bản hiện tại không
 # case "$UBUNTU_VERSION" in
@@ -25,11 +25,12 @@ REPO_URL="https://raw.githubusercontent.com/diepxuan/ppa"
 # esac
 
 # Thêm khoá GPG
-# curl -fsSL "$KEY_URL" | sudo gpg --dearmor -o /usr/share/keyrings/ppa.gpg
+curl -fsSL "$KEY_URL" | sudo gpg --dearmor -o /usr/share/keyrings/diepxuan.gpg
 
 # Thêm repository vào sources.list.d
 # echo "deb [signed-by=/usr/share/keyrings/ppa.gpg] $REPO_URL $UBUNTU_VERSION main" | sudo tee /etc/apt/sources.list.d/ppa.list
-echo "deb [trusted=yes] $REPO_URL $CODENAME main" | sudo tee /etc/apt/sources.list.d/diepxuan.list
+# echo "deb [trusted=yes] $REPO_URL $CODENAME main" | sudo tee /etc/apt/sources.list.d/diepxuan.list
+echo "deb [signed-by=/usr/share/keyrings/diepxuan.gpg] $REPO_URL $CODENAME main" | sudo tee /etc/apt/sources.list.d/diepxuan.list
 
 # Cập nhật và cài đặt gói
 sudo apt-get update
