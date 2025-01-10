6393a811e1884bdb0762d2219e0f0995c1123498
Trần Ngọc Đức <caothu91@gmail.com>
2025-01-10 08:36:51 +0700

    update bash install script

--- README.md
+++ README.md
@@ -1,24 +1,7 @@
 A PPA repository for DiepXuan Personal Package Archives
 
-#### Use Apt package manager
-
-```bash
-sudo add-apt-repository ppa:caothu91/ppa
-```
-
 #### Use own custom installer
 
 ```bash
 curl -s https://raw.githubusercontent.com/diepxuan/ppa/main/install.sh | bash
-```
-
-#### Manual install
-
-```bash
-apt install software-properties-common
-add-apt-repository ppa:ondrej/php -y
-
-# Install package
-apt update
-apt install ductn
 ```
--- install.sh
+++ install.sh
@@ -1,4 +1,36 @@
-apt install -y software-properties-common
-add-apt-repository ppa:caothu91/ppa -y
-apt update
-apt install -y ductn
+#!/usr/bin/env bash
+
+[[ "$OSTYPE" == "darwin"* ]] &&
+    CODENAME=$(sw_vers -productVersion | awk -F '.' '{print $1"."$2}')
+[[ -f /etc/os-release ]] && . /etc/os-release
+[[ -f /etc/lsb-release ]] && . /etc/lsb-release
+CODENAME=${CODENAME:-$DISTRIB_CODENAME}
+CODENAME=${CODENAME:-$VERSION_CODENAME}
+CODENAME=${CODENAME:-$UBUNTU_CODENAME}
+CODENAME=${CODENAME:-"unknown"}
+
+# Xác định URL của repository
+REPO_URL="https://raw.githubusercontent.com/diepxuan/ppa"
+# KEY_URL="$REPO_URL/public.key"
+
+# Kiểm tra xem có hỗ trợ phiên bản hiện tại không
+# case "$UBUNTU_VERSION" in
+#   focal|jammy|bionic)
+#     echo "Adding PPA for $UBUNTU_VERSION..."
+#     ;;
+#   *)
+#     echo "Error: $UBUNTU_VERSION is not supported."
+#     exit 1
+#     ;;
+# esac
+
+# Thêm khoá GPG
+# curl -fsSL "$KEY_URL" | sudo gpg --dearmor -o /usr/share/keyrings/ppa.gpg
+
+# Thêm repository vào sources.list.d
+# echo "deb [signed-by=/usr/share/keyrings/ppa.gpg] $REPO_URL $UBUNTU_VERSION main" | sudo tee /etc/apt/sources.list.d/ppa.list
+echo "deb [trusted=yes] $REPO_URL $CODENAME main" | sudo tee /etc/apt/sources.list.d/diepxuan.list
+
+# Cập nhật và cài đặt gói
+sudo apt-get update
+# echo "PPA added successfully. Now you can install packages using 'sudo apt install <package-name>'."
