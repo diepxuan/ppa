648e26474f018d83847a9e0319b297d8fa7d90e8
Tran Ngoc Duc <caothu91@gmail.com>
2025-01-17 14:44:36 +0700

    Enhance install script with sudo handling and GPG key installation checks

--- install.sh
+++ install.sh
@@ -9,6 +9,18 @@ CODENAME=${CODENAME:-$VERSION_CODENAME}
 CODENAME=${CODENAME:-$UBUNTU_CODENAME}
 CODENAME=${CODENAME:-"unknown"}
 
+run_as_sudo() {
+    _SUDO=sudo
+    command -v sudo &>/dev/null || _SUDO=''
+    echo "Running as sudo: $*"
+    if [[ $EUID -ne 0 ]]; then
+        $_SUDO $@
+    else
+        $@
+    fi
+}
+SUDO=${SUDO:-'run_as_sudo'}
+
 # Xác định URL của repository
 REPO_URL="https://ppa.diepxuan.com/"
 KEY_URL="$REPO_URL/key.gpg"
@@ -25,13 +37,23 @@ KEY_URL="$REPO_URL/key.gpg"
 # esac
 
 # Thêm khoá GPG
-curl -fsSL "$KEY_URL" | sudo gpg --dearmor -o /usr/share/keyrings/diepxuan.gpg
+# Kiểm tra gpg có sẵn hay không
+if ! command -v gpg &>/dev/null; then
+    # Kiểm tra hệ điều hành và cài đặt tương ứng
+    if [[ -f /etc/debian_version ]]; then
+        # Dành cho Ubuntu/Debian
+        $SUDO apt update
+        $SUDO apt install -y gnupg
+    fi
+fi
+
+curl -fsSL "$KEY_URL" | $SUDO gpg --dearmor -o /usr/share/keyrings/diepxuan.gpg
 
 # Thêm repository vào sources.list.d
-# echo "deb [signed-by=/usr/share/keyrings/ppa.gpg] $REPO_URL $UBUNTU_VERSION main" | sudo tee /etc/apt/sources.list.d/ppa.list
-# echo "deb [trusted=yes] $REPO_URL $CODENAME main" | sudo tee /etc/apt/sources.list.d/diepxuan.list
-echo "deb [signed-by=/usr/share/keyrings/diepxuan.gpg] $REPO_URL $CODENAME main" | sudo tee /etc/apt/sources.list.d/diepxuan.list
+# echo "deb [signed-by=/usr/share/keyrings/ppa.gpg] $REPO_URL $UBUNTU_VERSION main" | $SUDO tee /etc/apt/sources.list.d/ppa.list
+# echo "deb [trusted=yes] $REPO_URL $CODENAME main" | $SUDO tee /etc/apt/sources.list.d/diepxuan.list
+echo "deb [signed-by=/usr/share/keyrings/diepxuan.gpg] $REPO_URL $CODENAME main" | $SUDO tee /etc/apt/sources.list.d/diepxuan.list
 
 # Cập nhật và cài đặt gói
-sudo apt-get update
+$SUDO apt-get update
 # echo "PPA added successfully. Now you can install packages using 'sudo apt install <package-name>'."
