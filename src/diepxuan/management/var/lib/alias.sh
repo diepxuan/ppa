18bdec9b640c25ffe0b0fdf8465db1dfcb6d587d
diepxuan <ductn@diepxuan.com>
2025-02-14 17:21:42 +0000

    Add ductn-ll package and enhance ductn functionality with daily updates

---
+++ src/diepxuan/management/var/lib/alias.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#!/bin/bash
+
+d_alias:ll() {
+    [[ "$1" == "--help" ]] &&
+        echo "Alias script ll. Displays directory contents in long format (like ls -la)." &&
+        return
+
+    # Kiểm tra xem alias ll có tồn tại không
+    if alias ll >/dev/null 2>&1; then
+        # Nếu có alias, sử dụng alias
+        # command ll "$@"
+        eval "ll $@"
+    else
+        # Nếu không có alias, sử dụng lệnh ls -la
+        ls -la "$@"
+    fi
+}
+
+--isenabled() {
+    echo '1'
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    "$@"
+fi
